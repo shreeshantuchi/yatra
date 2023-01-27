8f54cd1045a7abb7c9c28436d614f8906b0d1c89
shreeshantuchi <82638369+shreeshantuchi@users.noreply.github.com>
2023-01-27 11:41:58 +0545

    updated location services

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:yatra/screens/home_screen/widget/home_carousel.dart';
+import 'package:yatra/services/location_services.dart';
 import 'package:yatra/utils/colors.dart';
+import 'package:yatra/utils/routes.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  List<String> imagePaths = ["assets/1.jpeg", "assets/2.jpeg", "assets/3.jpeg"];
+  HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<String> imagePaths = ["assets/1.jpeg", "assets/2.jpeg", "assets/3.jpeg"];
+  @override
+  void initState() {
+    // TODO: implement initState
+    context.read<LocationService>().determinePosition();
+    context.read<LocationService>().listenToLocationChange();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(height: 20.h),
             const SearchForm(),
             SizedBox(height: 20.h),
-            HomeCarousel(imagePaths: imagePaths)
+            HomeCarousel(imagePaths: widget.imagePaths)
           ],
         ),
       ),
@@ -49,9 +58,7 @@ class WelcomeHeadingText extends StatelessWidget {
             SizedBox(
               height: 5.h,
             ),
-            currentLocation(
-              context,
-            ),
+            const CurrentLcoation()
           ],
         ),
         menuIcons(context),
@@ -70,24 +77,6 @@ class WelcomeHeadingText extends StatelessWidget {
           PhosphorIcons.handWavingFill,
           color: Colors.amber,
         ),
-      ],
-    );
-  }
-
-  Widget currentLocation(BuildContext context) {
-    return Row(
-      children: [
-        const Icon(
-          PhosphorIcons.mapPinBold,
-          color: MyColor.greyColor,
-        ),
-        SizedBox(
-          width: 5.w,
-        ),
-        Text(
-          "Kathamandu, Nepal",
-          style: Theme.of(context).textTheme.bodyText2,
-        )
       ],
     );
   }
@@ -122,6 +111,37 @@ class SearchForm extends StatelessWidget {
             hintText: "Search",
             hintStyle: Theme.of(context).textTheme.bodyText2,
             border: InputBorder.none),
+      ),
+    );
+  }
+}
+
+class CurrentLcoation extends StatefulWidget {
+  const CurrentLcoation({super.key});
+
+  @override
+  State<CurrentLcoation> createState() => _CurrentLcoationState();
+}
+
+class _CurrentLcoationState extends State<CurrentLcoation> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, MyRoutes.locationRoute),
+      child: Row(
+        children: [
+          const Icon(
+            PhosphorIcons.mapPinBold,
+            color: MyColor.greyColor,
+          ),
+          SizedBox(
+            width: 5.w,
+          ),
+          context.watch<LocationService>().placemark.isNotEmpty
+              ? Text(
+                  "${context.watch<LocationService>().placemark.last.locality} , ${context.watch<LocationService>().placemark.last.country}")
+              : CircularProgressIndicator(),
+        ],
       ),
     );
   }
