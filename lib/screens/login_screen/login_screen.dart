18eddd5abe24bf0fcef01be7cf183bf3a96477e5
shreeshantuchi <82638369+shreeshantuchi@users.noreply.github.com>
2023-02-08 07:59:24 +0545

    validator added in form

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -11,150 +11,168 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return customBackground(
       child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 36.w, vertical: 20.h),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 66.h, bottom: 100.h),
-                    child: Row(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 66.h, bottom: 100.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/logo.png",
+                            height: 95.h,
+                            width: 235.5.h,
+                            fit: BoxFit.contain,
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Text(
+                    //   "Login Now",
+                    //   style: Theme.of(context).textTheme.headline1,
+                    // ),
+                    // Padding(
+                    //   padding: EdgeInsets.symmetric(vertical: 30.h),
+                    //   child: Text(
+                    //     "Please enter the credential below to continue",
+                    //     style: Theme.of(context).textTheme.bodyText2,
+                    //   ),
+                    // ),
+                    Text(
+                      "Your Email Address",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter an email';
+                        }
+                        if (!RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      style: Theme.of(context).textTheme.bodyText1,
+                      decoration: FormStyle.signUpStyle(
+                          context: context, hintText: "example@gmail.com"),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Text(
+                      "Your Password",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    TextFormField(
+                      style: Theme.of(context).textTheme.bodyText1,
+                      obscureText: true,
+                      decoration: FormStyle.signUpStyle(
+                          context: context, hintText: "password"),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: (() => Navigator.pushNamed(
+                              context, MyRoutes.registerRoute)),
+                          child: Text(
+                            "Don't have an account? \nRegister",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(color: MyColor.whiteColor),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.h),
+                          child: Text(
+                            "Forgot Password?",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(color: MyColor.whiteColor),
+                          ),
+                        )
+                      ],
+                    ),
+                    CustomButton(
+                        radius: 30.sp,
+                        text: "Conitinue",
+                        textColor: MyColor.blackColor,
+                        color: MyColor.whiteColor.withOpacity(0.5),
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            Navigator.pushNamed(context, MyRoutes.landRoute);
+                            // Form is valid, process data.
+                          }
+                        }),
+                    SizedBox(
+                      height: 25.h,
+                    ),
+
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Image.asset(
-                          "assets/logo.png",
-                          height: 95.h,
-                          width: 235.5.h,
-                          fit: BoxFit.contain,
+                        Container(
+                          height: 1.h,
+                          width: 150.w,
+                          color: MyColor.whiteColor.withOpacity(0.5),
+                        ),
+                        Text(" OR ",
+                            style: Theme.of(context).textTheme.bodyText2),
+                        Container(
+                          height: 1.h,
+                          width: 150.w,
+                          color: MyColor.whiteColor.withOpacity(0.5),
                         ),
                       ],
                     ),
-                  ),
-                  // Text(
-                  //   "Login Now",
-                  //   style: Theme.of(context).textTheme.headline1,
-                  // ),
-                  // Padding(
-                  //   padding: EdgeInsets.symmetric(vertical: 30.h),
-                  //   child: Text(
-                  //     "Please enter the credential below to continue",
-                  //     style: Theme.of(context).textTheme.bodyText2,
-                  //   ),
-                  // ),
-                  Text(
-                    "Your Email Address",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  TextFormField(
-                    style: Theme.of(context).textTheme.bodyText1,
-                    decoration: FormStyle.signUpStyle(
-                        context: context, hintText: "example@gmail.com"),
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                  Text(
-                    "Your Password",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  TextFormField(
-                    style: Theme.of(context).textTheme.bodyText1,
-                    obscureText: true,
-                    decoration: FormStyle.signUpStyle(
-                        context: context, hintText: "password"),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        onTap: (() => Navigator.pushNamed(
-                            context, MyRoutes.registerRoute)),
-                        child: Text(
-                          "Don't have an account? \nRegister",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(color: MyColor.whiteColor),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.h),
-                        child: Text(
-                          "Forgot Password?",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(color: MyColor.whiteColor),
-                        ),
-                      )
-                    ],
-                  ),
-                  CustomButton(
-                    radius: 30.sp,
-                    text: "Conitinue",
-                    textColor: MyColor.blackColor,
-                    color: MyColor.whiteColor.withOpacity(0.5),
-                    onTap: () =>
-                        Navigator.pushNamed(context, MyRoutes.landRoute),
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 1.h,
-                        width: 150.w,
-                        color: MyColor.whiteColor.withOpacity(0.5),
-                      ),
-                      Text(" OR ",
-                          style: Theme.of(context).textTheme.bodyText2),
-                      Container(
-                        height: 1.h,
-                        width: 150.w,
-                        color: MyColor.whiteColor.withOpacity(0.5),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                  CustomButton(
-                    imgIcon: "assets/google.png",
-                    radius: 30.sp,
-                    borderEnabled: true,
-                    text: "Log in with Google",
-                    textColor: MyColor.whiteColor,
-                    color: Colors.transparent,
-                    onTap: () =>
-                        Navigator.pushNamed(context, MyRoutes.homeRoute),
-                  ),
-                  SizedBox(
-                    height: 48.h,
-                  ),
-                  CustomButton(
-                    imgIcon: "assets/apple.png",
-                    borderEnabled: true,
-                    radius: 30.sp,
-                    text: "Sign up with Apple",
-                    textColor: MyColor.whiteColor,
-                    color: Colors.transparent,
-                    onTap: () =>
-                        Navigator.pushNamed(context, MyRoutes.homeRoute),
-                  ),
-                ],
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    CustomButton(
+                      imgIcon: "assets/google.png",
+                      radius: 30.sp,
+                      borderEnabled: true,
+                      text: "Log in with Google",
+                      textColor: MyColor.whiteColor,
+                      color: Colors.transparent,
+                      onTap: () =>
+                          Navigator.pushNamed(context, MyRoutes.homeRoute),
+                    ),
+                    SizedBox(
+                      height: 48.h,
+                    ),
+                    CustomButton(
+                      imgIcon: "assets/apple.png",
+                      borderEnabled: true,
+                      radius: 30.sp,
+                      text: "Sign up with Apple",
+                      textColor: MyColor.whiteColor,
+                      color: Colors.transparent,
+                      onTap: () =>
+                          Navigator.pushNamed(context, MyRoutes.homeRoute),
+                    ),
+                  ],
+                ),
               ),
             ),
           )),
--- lib/screens/register_screen/register_screen.dart
+++ lib/screens/register_screen/register_screen.dart
@@ -14,6 +14,8 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? _password;
   @override
   Widget build(BuildContext context) {
     return customBackground(
@@ -22,91 +24,144 @@ class _RegisterScreenState extends State<RegisterScreen> {
           body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 36.w, vertical: 20.h),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 66.h, bottom: 100.h),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          "assets/logo.png",
-                          height: 95.h,
-                          width: 235.5.h,
-                          fit: BoxFit.contain,
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Text(
-                  //   "Login Now",
-                  //   style: Theme.of(context).textTheme.headline1,
-                  // ),
-                  // Padding(
-                  //   padding: EdgeInsets.symmetric(vertical: 30.h),
-                  //   child: Text(
-                  //     "Please enter the credential below to continue",
-                  //     style: Theme.of(context).textTheme.bodyText2,
-                  //   ),
-                  // ),
-                  Text(
-                    "Your Email Address",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  TextFormField(
-                    style: Theme.of(context).textTheme.bodyText1,
-                    decoration: FormStyle.signUpStyle(
-                        context: context, hintText: "example@gmail.com"),
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                  Text(
-                    "Your Password",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  TextFormField(
-                    style: Theme.of(context).textTheme.bodyText1,
-                    obscureText: true,
-                    decoration: FormStyle.signUpStyle(
-                        context: context, hintText: "password"),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 30.h),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        GestureDetector(
-                          child: Text(
-                            "Already have an account? \nLogin",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .copyWith(color: MyColor.whiteColor),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 66.h, bottom: 100.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/logo.png",
+                            height: 95.h,
+                            width: 235.5.h,
+                            fit: BoxFit.contain,
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  CustomButton(
-                    radius: 30.sp,
-                    text: "Sign Up",
-                    textColor: MyColor.blackColor,
-                    color: MyColor.whiteColor.withOpacity(0.5),
-                    onTap: () =>
-                        Navigator.pushNamed(context, MyRoutes.landRoute),
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                ],
+                    // Text(
+                    //   "Login Now",
+                    //   style: Theme.of(context).textTheme.headline1,
+                    // ),
+                    // Padding(
+                    //   padding: EdgeInsets.symmetric(vertical: 30.h),
+                    //   child: Text(
+                    //     "Please enter the credential below to continue",
+                    //     style: Theme.of(context).textTheme.bodyText2,
+                    //   ),
+                    // ),
+                    Text(
+                      "Your Email Address",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    TextFormField(
+                      validator: ((value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter an email';
+                        }
+                        if (!RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      }),
+                      style: Theme.of(context).textTheme.bodyText1,
+                      decoration: FormStyle.signUpStyle(
+                          context: context, hintText: "example@gmail.com"),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Text(
+                      "Password",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    TextFormField(
+                      onSaved: (newValue) => _password = newValue,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        }
+                        if (value.length < 8) {
+                          return "Password should be minimum of 8 characters";
+                        }
+                      },
+                      style: Theme.of(context).textTheme.bodyText1,
+                      obscureText: true,
+                      decoration: FormStyle.signUpStyle(
+                          context: context, hintText: "Enter your password"),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Text(
+                      "Password",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    TextFormField(
+                      validator: ((value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        }
+                        if (value.length < 8) {
+                          return "Password should be minimum of 8 characters";
+                        }
+                        if (value != _password) {
+                          return "Password doesnt match";
+                        }
+                      }),
+                      style: Theme.of(context).textTheme.bodyText1,
+                      obscureText: true,
+                      decoration: FormStyle.signUpStyle(
+                          context: context, hintText: "Retype your password"),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 30.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: (() => Navigator.of(context).pop()),
+                            child: Text(
+                              "Already have an account? \nLogin",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(color: MyColor.whiteColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    CustomButton(
+                        radius: 30.sp,
+                        text: "Sign Up",
+                        textColor: MyColor.blackColor,
+                        color: MyColor.whiteColor.withOpacity(0.5),
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            Navigator.pushNamed(context, MyRoutes.landRoute);
+                          }
+                        }),
+                    SizedBox(
+                      height: 25.h,
+                    ),
+                  ],
+                ),
               ),
             ),
           )),
